01030c738dfd507d381ef5ae8476dd4cc836ac48
Qboooogle <1432500814@qq.com>
2016-11-23 17:44:46 +0800

    完结

--- wxapp1/pages/index/index.js
+++ wxapp1/pages/index/index.js
@@ -2,22 +2,71 @@
  * @Author: QBoooogle
  * @Date:   2016-11-23 15:18:31
  * @Last Modified by:   QBoooogle
- * @Last Modified time: 2016-11-23 15:32:04
+ * @Last Modified time: 2016-11-23 16:37:48
  */
 
 'use strict';
 var app = getApp();
-onMenuShareTimeline: function() {
-	return {
-		title: "首页"
-	}
-}
-onLoad: function(opstions) {
-	console.log("--------------onload---------------");
-}
-onReady: function() {
-	console.log("--------------ready---------------");
-}
 Page({
-
+	data: {
+		name: "qiangbo",
+		condition: false,
+		msg: "this is a template",
+		time: "2016-11-23",
+		index: "0",
+		array: [{
+			message: 'foo',
+		}, {
+			message: 'bar'
+		}],
+		objectArray: [{
+			id: 5,
+			unique: 'unique_5'
+		}, {
+			id: 4,
+			unique: 'unique_4'
+		}, {
+			id: 3,
+			unique: 'unique_3'
+		}, {
+			id: 2,
+			unique: 'unique_2'
+		}, {
+			id: 1,
+			unique: 'unique_1'
+		}, {
+			id: 0,
+			unique: 'unique_0'
+		}],
+		numberArray: [1, 2, 3, 4]
+	},
+	switch: function(e) {
+		const length = this.data.objectArray.length
+		for (let i = 0; i < length; ++i) {
+			const x = Math.floor(Math.random() * length)
+			const y = Math.floor(Math.random() * length)
+			const temp = this.data.objectArray[x]
+			this.data.objectArray[x] = this.data.objectArray[y]
+			this.data.objectArray[y] = temp
+		}
+		this.setData({
+			objectArray: this.data.objectArray
+		})
+	},
+	addToFront: function(e) {
+		const length = this.data.objectArray.length
+		this.data.objectArray = [{
+			id: length,
+			unique: 'unique_' + length
+		}].concat(this.data.objectArray)
+		this.setData({
+			objectArray: this.data.objectArray
+		})
+	},
+	addNumberToFront: function(e) {
+		this.data.numberArray = [this.data.numberArray.length + 1].concat(this.data.numberArray)
+		this.setData({
+			numberArray: this.data.numberArray
+		})
+	})
 })
