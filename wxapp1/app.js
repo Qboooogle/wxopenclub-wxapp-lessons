1bede500ab192685017c47cf2ae220fdade26baa
Qboooogle <1432500814@qq.com>
2016-11-23 15:38:52 +0800

    初始demo

---
+++ wxapp1/app.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: QBoooogle
+ * @Date:   2016-11-23 15:20:24
+ * @Last Modified by:   QBoooogle
+ * @Last Modified time: 2016-11-23 15:21:19
+ */
+
+'use strict';
+App({
+
+})
---
+++ wxapp1/pages/index/index.js
@@ -0,0 +1,23 @@
+/*
+ * @Author: QBoooogle
+ * @Date:   2016-11-23 15:18:31
+ * @Last Modified by:   QBoooogle
+ * @Last Modified time: 2016-11-23 15:32:04
+ */
+
+'use strict';
+var app = getApp();
+onMenuShareTimeline: function() {
+	return {
+		title: "首页"
+	}
+}
+onLoad: function(opstions) {
+	console.log("--------------onload---------------");
+}
+onReady: function() {
+	console.log("--------------ready---------------");
+}
+Page({
+
+})
